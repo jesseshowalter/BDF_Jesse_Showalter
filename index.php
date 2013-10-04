fc74bf9f16a37e04982d844fe0383adffef28e30
Jesse Showalter <jesseshowalter@fullsail.edu>
2013-10-03 15:13:36 -1000

    Created a simple time counter in the index and some simple CSS

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>test</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<div class="wrapper">
+		<h1>Jesse Showalter</h1>
+		<p><?php 
+			echo "Hello everyone, the current time in Hawaii is" . " " . date("D M d, Y G:i a") ;
+			?>
+		</p>
+	</div>
+</body>
+</html>
