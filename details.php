b5f65340d429f4e6fab6690c5170a1395ddbb6a9
Jesse Showalter <jesseshowalter@fullsail.edu>
2013-10-11 14:29:55 -1000

    created taskmodel.php

---
+++ details.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<title>BDF - Jesse Showalter</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+
+
+<!--
+		<?php 
+			$dsn = "mysql:host=127.0.0.1;port=8889;dbname=bdf1310";
+			$db_user = "root";
+			$db_pass = "root";
+			$db = new \PDO($dsn, $db_user, $db_pass);
+			$statement = $db->prepare("
+				SELECT taskTitle, taskDescription
+				FROM taskInfo
+			");
+			if ($statement->execute()){
+				$rows = $statement->fetchAll(\PDO::FETCH_ASSOC);
+				//var_dump($statement);
+				foreach ($rows as $num => $row) {
+					echo "taskTitle";
+				};
+
+			};
+		 ?>
+-->
+
+</body>
+</html>
