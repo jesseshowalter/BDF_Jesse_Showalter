946c04429f8105ff16726f971ab78e0c9af6d7e0
Jesse Showalter <jesseshowalter@fullsail.edu>
2013-10-14 16:07:42 -1000

    reallocated my index page to be my login screen

--- index.php
+++ index.php
@@ -1,105 +1,45 @@
-<?php include ("models/OpenConnection.php"); ?>
-<?php  
-	// 2. Perform databse query
-	$honeydo = "SELECT * ";
-	$honeydo .= "FROM taskInfo ";
-	//$honeydo .= "WHERE taskGroup_id = 3 ";
+<div class="welcomepage">
+	<?php include ('views/header.php'); ?> 
+</div>	
+	<article id="examples" class="stage">
 
-
-	$result = mysqli_query($connection, $honeydo);
-	// test if there was a query error
-	if(!$result){
-		die("Database query failed.");
-	}
-
-?>
-<!DOCTYPE html>
-
-<html lang="en">
-
-	<head>
-		<meta charset="utf-8" />
-		<title>MEDO | A visual todo app for the nerd on the go</title>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta name="title" content="MEDO | A visual todo app for the nerd on the go" />
-		<meta name="description" content="A visual todo app for the nerd on the go." />
-		<meta name="keywords" content="MEDO, todo, application, nerds, fullsail" />
-		<meta name="viewport" content="initial-scale=1,minimum-scale=1,width=device-width">
-
-	    <!-- Javascript -->
-	    <script type="text/javascript" src="/js/jquery-1.8.3.min.js"></script>
-
-	    <!-- Stylesheets -->
-	    <link href='http://fonts.googleapis.com/css?family=Raleway:500,900,200' rel='stylesheet' type='text/css'>
-		<link rel="stylesheet" href="assets/css/index.css" type="text/css" media="screen" />
-
-		<!--[if lt IE 9]>
-			<script type="text/javascript" src="/js/html5shiv.js"></script>
-			<script type="text/javascript" src="/js/selectivizr.js"></script>
-			<script type="text/javascript" src="/js/respond.js"></script>
-		<![endif]-->
-	</head>
-
-	<body>
-
-		<header>
-			<h1><a href="#"><span>ME</span>DO</a></h1>
-			<h2>A simple task list</h2>
-		</header>
-
-		<!-- Features -->	
-
-		<article id="examples" class="stage">
-			<h3>Add New Task</h3>
-			<a class="button" href="#">Click to add</a>
-
-
-			<ul>
-
-
-
-				<?php  
-					// 3. Use return data (if any)
-					while($taskList = mysqli_fetch_assoc($result)) {
-						// output data from each row
-				?>
-				<?php $link_name = "Details"; ?>
-				<?php $id = task; ?>
-					<li>
-						<a href="details.php?id=<?php echo $id; ?>">
-							<img src="<?php echo $taskList["taskPhoto"]; ?>" />
-							<h4><?php echo $taskList["taskTitle"]; ?></h4>
-							<span><?php echo $taskList["taskDescription"]; ?></span>
-						</a>
-					</li>
-
-					
-				<?php 
-					};
-				?>
-				</ul>
-				<?php  
-					// 4. Release returned data
-					mysqli_free_result($result);
-				?>
-
-			</ul>
-		</article>
-
-
+      <div class="jumbotron">
+		<h4>Sign In</h4>
+		<?php $message = $_GET['message'];
 		
-		<footer>
-			<div class="center">
-				<h3>BUILT BY</h3>
-				<section id="company">
-					<a href="http://www.jesseshowalter.com">Jesse Showalter</a>
-				</section>
-			</div>
-		</footer>
+		
+		//Alert messages based on integers
+		if($message == 1) {
+		echo '
+		<div class="alert">
+		  <button type="button" class="close" data-dismiss="alert">&times;</button>
+		  <strong>Invalid username or password</strong>
+		</div>
+		';
+		}
+		
+		else if($message == 2) {
+		echo '
+		<div class="alert alert-success">
+		  <button type="button" class="close" data-dismiss="alert">&times;</button>
+		  <strong>You have successfully logged out! </strong>
+		</div>
+		';
+		}
+		
+		?>
+		
+		
+		<form action="auth_check.php" method="post" class="form-signin">
+			<input name="username" type="text" class="input-block-level" placeholder="Username">
+			<input name="password" type="password" class="input-block-level" placeholder="Password"> 
+			<button class="button" type="submit">Sign in</button>	
+		</form>
+	  
+        <a class="button" href="register.php">Register New User</a>
+      </div>
 
-	</body>
-
-</html>
+    </article>
 
 
-<?php include ("models/CloseConnection.php"); ?>
+<?php include ('views/footer.php'); ?> 
