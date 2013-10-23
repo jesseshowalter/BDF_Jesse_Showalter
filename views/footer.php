480f5602e27a348a3ffabb4467b7d05ae143019a
Jesse Showalter <jesseshowalter@fullsail.edu>
2013-10-22 16:18:00 -1000

    changed footer link

--- views/footer.php
+++ views/footer.php
@@ -2,7 +2,7 @@
 			<div class="center">
 				<h3>BUILT BY</h3>
 				<section id="company">
-					<a href="http://www.jesseshowalter.com">Jesse Showalter</a>
+					<a href="http://www.jesseshowalter.com" target="_blank">Jesse Showalter</a>
 				</section>
 			</div>
 		</footer>
